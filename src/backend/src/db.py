03d2c8097bd0477036849db8e5d7efd2f5780746
xkunD <103330637+xkunD@users.noreply.github.com>
2024-04-28 13:12:25 -0400

    serialize post user comments

--- src/backend/src/db.py
+++ src/backend/src/db.py
@@ -13,6 +13,7 @@ class User(db.Model):
     first_name= db.Column(db.String, nullable=False)
     last_name= db.Column(db.String, nullable=False)
     username= db.Column(db.String, nullable=False)
+    posts = db.relationship('Post', backref='user', lazy=True)
 
     def __init__(self, **kwargs):
         self.first_name= kwargs.get("first_name")
@@ -20,7 +21,13 @@ class User(db.Model):
         self.username= kwargs.get("username")
     
     def serialize(self):
-        pass
+        return {
+            'id': self.id,
+            'first name': self.first_name,
+            'last name': self.last_name,
+            'username': self.username
+        }
+
 
 class Post(db.Model):
     """
@@ -28,26 +35,40 @@ class Post(db.Model):
     """
 
     __tablename__="post"
-    id=db.Column(db.Integer, primary_key=True, autoincrement=True)
-    title= db.Column(db.String, nullable=False)
-    ingredients= db.Column(db.String, nullable=False)
-    image= db.Column(db.String, nullable=False)
-    meal_type= db.Column(db.String, nullable=False)
-    date= db.Column(db.String, nullable=False)
-    likes= db.Column(db.Integer, nullable=False)
-    user_id= db.Column(db.Integer, db.ForeignKey("user.id"))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String, nullable=False)
+    ingredients = db.Column(db.String, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    meal_type = db.Column(db.String, nullable=False)
+    difficulty = db.Column(db.String, nullable=False)
+    date = db.Column(db.String, nullable=False)
+    likes = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
 
     def __init__(self, **kwargs):
-        title= kwargs.get("title")
-        ingredients= kwargs.get("ingredients")
-        image= kwargs.get("image")
-        meal_type= kwargs.get("meal_type")
-        date= kwargs.get("date")
-        likes= kwargs.get("likes")
-        user_id= kwargs.get("user_id")
+        self.title= kwargs.get("title")
+        self.ingredients= kwargs.get("ingredients")
+        self.image= kwargs.get("image")
+        self.meal_type= kwargs.get("meal_type")
+        self.difficulty = kwargs.get("difficulty")
+        self.date= kwargs.get("date")       # date format?
+        self.user_id= kwargs.get("user_id")
+        self.likes= kwargs.get("likes")
+        self.comments = db.relationship('Comment', backref='post', lazy=True)
     
     def serialize(self):
-        pass
+        return {
+            'id': self.id,
+            'title': self.title,
+            'ingredients': self.ingredients,
+            'image': self.image,
+            'meal_type': self.meal_type,
+            'difficulty': self.difficulty,
+            'date': self.date,
+            'user_id': self.user_id,
+            'likes': self.likes,
+            'comments': [comment.serialize() for comment in self.comments]
+        }
 
 class Comment(db.Model):
     """
@@ -55,15 +76,24 @@ class Comment(db.Model):
     """
 
     __tablename__= "comment"
-    id= db.Column(db.Integer, primary_key=True, autoincrement=True)
-    message= db.Column(db.Integer, nullable=False)
-    post_id= db.Column(db.Integer, db.ForeignKey("post.id"))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    post_id = db.Column(db.Integer, db.ForeignKey("post.id"), nullable=False)
+    # user_id = 
+    date = db.Column(db.String, nullable=False)
+    message = db.Column(db.String, nullable=False)
+
 
     def __init__(self, **kwargs):
-        self.message= kwargs.get("message")
-        self.post_id= kwargs.get("post_id")
+        self.message = kwargs.get("message")
+        self.post_id = kwargs.get("post_id")
+        self.date = kwargs.get("date")
 
     def serialize(self):
-        pass
+        return {
+            'id': self.id,
+            'post_id': self.post_id,
+            'date': self.date,
+            'message': self.message,
+        }
 
 
